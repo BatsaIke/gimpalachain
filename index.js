d10da0bd0204eea2ea7df72f6786cd727348e656
IsaacKimo <139249129+IsaacKimo@users.noreply.github.com>
2023-11-13 18:27:23 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import { OpenAI } from 'langchain/llms';
+import { RetrievalQAChain } from 'langchain/chains';
+import { HNSWLib } from 'langchain/vectorstores';
+import { OpenAIEmbeddings } from 'langchain/embeddings';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const txtFilename = "data";
+const txtPath = `./${txtFilename}.txt`;
+const VECTOR_STORE_PATH = `${txtFilename}.index`;
+
+app.post('/ask', async (req, res) => {
+  const { question } = req.body;
+
+  if (!question) {
+    return res.status(400).json({ error: 'Question is required in the request body.' });
+  }
+
+  const model = new OpenAI({});
+  let vectorStore;
+
+  if (fs.existsSync(VECTOR_STORE_PATH)) {
+    console.log('Vector Exists..');
+    vectorStore = await HNSWLib.load(VECTOR_STORE_PATH, new OpenAIEmbeddings());
+  } else {
+    const text = fs.readFileSync(txtPath, 'utf8').replace(/\r/g, ''); // Use replace with a regular expression
+    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+    const docs = await textSplitter.createDocuments([text]);
+    vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+    await vectorStore.save(VECTOR_STORE_PATH);
+  }
+
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+
+  try {
+    const result = await chain.call({
+      query: question,
+    });
+    res.json({ response: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+ 
